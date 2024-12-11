bac7692fcfa880ba9e79519cb48673a9d411227c
Sardius <0xsardius@gmail.com>
2024-12-10 20:48:17 -0500

    events built

--- src/SimplePredictionMarket.sol
+++ src/SimplePredictionMarket.sol
@@ -6,5 +6,60 @@ import {Ownable} from "@thirdweb-dev/contracts/extension/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract SimplePredictionMarket is Ownable, ReentrancyGuard {
+
+    enum MarketOutcome {
+        UNRESOLVED,
+        OPTION_A,
+        OPTION_B
+    }
+
+    struct Market {
+        string question;
+        uint256 endTime;
+        MarketOutcome outcome;
+        string optionA;
+        string optionB;
+        uint256 totalOptionAShares;
+        uint256 totalOptionBShares;
+        bool resolved;
+        mapping(address => uint256) optionASharesBalance;
+        mapping(address => uint256) optionBSharesBalance;
+
+        mapping(address => bool) hasClaimed;
+    }
+
+    IERC20 public bettingToken;
+    uint256 public marketCount;
+    mapping(uint256 => Market) public markets
+
+    event MarketCreated(
+        uint256 indexed marketId,
+        string question,
+        string optionA,
+        string optionB
+        uint256 endTime,
+        
+    );
+
+    event SharesPurchased(
+        uint256 indexed marketId,
+        address indexed buyer,
+        bool isOptionA,
+        uint256 amount,
+        
+    );
+
+    event MarketResolved(
+        uint256 indexed marketId,
+        MarketOutcome outcome
+    );
+
+    event Claimed(
+        uint256 indexed marketId,
+        address indexed user,
+        uint256 amount 
+    )
+
+
     constructor() {}
 }
